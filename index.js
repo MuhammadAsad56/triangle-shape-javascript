f068d52e15e7af910e6153f91b5c92313c3159dd
MuhammadAsad56 <ar535363@gmail.com>
2024-03-05 07:06:57 -0800

    message inserted

---
+++ index.js
@@ -0,0 +1,44 @@
+// // 1st task
+
+ var triangle = "";
+for (i=1; i <= 6; i++){
+    triangle = ""
+ for(j=1; j<=i; j++){
+    triangle +="*" ;
+ }
+console.log(triangle)
+}
+
+// 2nd task
+
+var number = " ";
+for (i=0; i <= 6; i++){
+    number = ""
+    for(j=0; j <= i; j++){
+        number += j
+    }
+    console.log(number)
+}
+
+// //  3rd task
+
+var triangle = ""
+
+for (var i = 7; i >= 1 ; i--){
+      triangle = ""
+    for (var j = 1; j <= i; j++){
+        triangle += "*"
+    }
+    console.log(triangle)
+}
+
+// // 4TH task
+
+var number =""
+for (var i = 7; i >= 1 ; i--){
+       number = ""
+    for (var j = 1; j <= i; j++){
+        number += j
+    }
+    console.log(number)
+}
